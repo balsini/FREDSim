bf5f89a9b5da3829fb7ca096da3a2e63d92606ce
Alessio Balsini <a.balsini@sssup.it>
2016-03-11 12:58:17 +0100

    Cleaned and compressed tasks declarations

--- examples/accelerated/accelerated.cpp
+++ examples/accelerated/accelerated.cpp
@@ -21,6 +21,8 @@
 using namespace MetaSim;
 using namespace RTSim;
 
+const unsigned int SW_TASK_NUM = 3;
+
 int main()
 {
   try {
@@ -34,19 +36,13 @@ int main()
     JSONTrace jtrace("trace.json");
     PSTrace pstrace("trace.pst");
 
-    StatMax soft_rt_max1("soft_rt_max1");
-    StatMean soft_rt_mean1("soft_rt_mean1");
-    StatMin soft_rt_min1("soft_rt_min1");
 
-    StatMax soft_rt_max2("soft_rt_max2");
-    StatMean soft_rt_mean2("soft_rt_mean2");
-    StatMin soft_rt_min2("soft_rt_min2");
+    StatMax soft_rt_max[SW_TASK_NUM];
+    StatMean soft_rt_mean[SW_TASK_NUM];
+    StatMin soft_rt_min[SW_TASK_NUM];
 
-    StatMax soft_rt_max3("soft_rt_max3");
-    StatMean soft_rt_mean3("soft_rt_mean3");
-    StatMin soft_rt_min3("soft_rt_min3");
 
-    cout << "Creating Scheduler and kernel" << endl;
+    cout << "Creating scheduler and kernel" << endl;
     FPScheduler softSched;
     RTKernel kern(&softSched);
 
@@ -62,104 +58,78 @@ int main()
     partition.push_back(FPGA_real.addPartition(1));
     ////////////////////
 
-    //RTKernel FPGA(&hardsched);
     //Resource res1("res1");
 
 
 
-    cout << "Creating the first task" << endl;
-    AcceleratedTask t1(10, 10, 0, "Task1");
-    cout << "Inserting code" << endl;
-    t1.insertCode("fixed(2);accelerate(2);fixed(1);");
+    cout << "Creating tasks" << endl;
 
-    vector<Scheduler *> t1_affinity = {partition.at(0)};
-    t1.getHW()->setAffinity(t1_affinity);
-    //t1.setAbort(false);
-    t1.addMaxRTStat(&soft_rt_max1);
-    t1.addMeanRTStat(&soft_rt_mean1);
-    t1.addMinRTStat(&soft_rt_min1);
+    vector<AcceleratedTask *> sw_tasks;
 
+    AcceleratedTask t0(10, 10, 0, "Task0");
+    AcceleratedTask t1(11, 11, 0, "Task1");
+    AcceleratedTask t2(20, 20, 0, "Task2");
 
-    cout << "Creating the second task" << endl;
-    AcceleratedTask t2(11, 11, 0, "Task2");
-    cout << "Inserting code" << endl;
-    t2.insertCode("fixed(1);accelerate(2);fixed(2);");
+    sw_tasks.push_back(&t0);
+    sw_tasks.push_back(&t1);
+    sw_tasks.push_back(&t2);
 
-    vector<Scheduler *> t2_affinity = {partition.at(0), partition.at(1)};
-    t2.getHW()->setAffinity(t2_affinity);
-    //t2.setAbort(false);
-    t2.addMaxRTStat(&soft_rt_max2);
-    t2.addMeanRTStat(&soft_rt_mean2);
-    t2.addMinRTStat(&soft_rt_min2);
+    cout << "Inserting codes" << endl;
 
-    cout << "Creating the third task" << endl;
-    AcceleratedTask t3(20, 20, 0, "Task3");
-    cout << "Inserting code" << endl;
-    t3.insertCode("fixed(2);accelerate(6);fixed(2);");
+    t0.insertCode("fixed(2);accelerate(2);fixed(1);");
+    t1.insertCode("fixed(1);accelerate(2);fixed(2);");
+    t2.insertCode("fixed(2);accelerate(6);fixed(2);");
 
-    vector<Scheduler *> t3_affinity = {partition.at(0), partition.at(1)};
-    t3.getHW()->setAffinity(t3_affinity);
-    //t2.setAbort(false);
-    t3.addMaxRTStat(&soft_rt_max3);
-    t3.addMeanRTStat(&soft_rt_mean3);
-    t3.addMinRTStat(&soft_rt_min3);
+    cout << "Setting hardware tasks affinities" << endl;
 
-    /*
-        cout << "Creating the third task" << endl;
-        AcceleratedTask t3(6, 6, 0, "Task2");
+    vector<Scheduler *> affinity;
 
-        cout << "Inserting code" << endl;
-        t3.insertCode("fixed(2);");
-        //t3.setAbort(false);
-*/
+    affinity = {partition.at(0)};
+    t0.getHW()->setAffinity(affinity);
+    affinity = {partition.at(0), partition.at(1)};
+    t1.getHW()->setAffinity(affinity);
+    affinity = {partition.at(0), partition.at(1)};
+    t2.getHW()->setAffinity(affinity);
 
+    cout << "Linking statistics accumulators" << endl;
+
+    for (unsigned int i=0; i<SW_TASK_NUM; ++i) {
+      sw_tasks.at(i)->addMaxRTStat(&soft_rt_max[i]);
+      sw_tasks.at(i)->addMeanRTStat(&soft_rt_mean[i]);
+      sw_tasks.at(i)->addMinRTStat(&soft_rt_min[i]);
+    }
 
     cout << "Setting up traces" << endl;
 
-    // new way
-    ttrace.attachToTask(&t1);
-    ttrace.attachToTask(&t2);
-    ttrace.attachToTask(&t3);
-
-    jtrace.attachToTask(&t1);
-    jtrace.attachToTask(&t2);
-    jtrace.attachToTask(&t3);
-
-    pstrace.attachToTask(&t1);
-    pstrace.attachToTask(&t2);
-    pstrace.attachToTask(&t3);
-    pstrace.attachToTask(t1.getHW());
-    pstrace.attachToTask(t2.getHW());
-    pstrace.attachToTask(t3.getHW());
+    for (unsigned int i=0; i<SW_TASK_NUM; ++i) {
+      ttrace.attachToTask(sw_tasks.at(i));
+      pstrace.attachToTask(sw_tasks.at(i));
+      pstrace.attachToTask(sw_tasks.at(i)->getHW());
+    }
 
     cout << "Adding tasks to schedulers" << endl;
 
-    kern.addTask(t1, "1");
-    kern.addTask(t2, "2");
-    kern.addTask(t3, "3");
-    FPGA_real.addTask(*(t1.getHW()), "");
-    FPGA_real.addTask(*(t2.getHW()), "");
-    FPGA_real.addTask(*(t3.getHW()), "");
-    //        kern.addTask(t3, "");
+    kern.addTask(t0, "1");
+    kern.addTask(t1, "2");
+    kern.addTask(t2, "3");
+
+    for (unsigned int i=0; i<SW_TASK_NUM; ++i) {
+      FPGA_real.addTask(*(sw_tasks.at(i)->getHW()), "");
+    }
 
     cout << "Ready to run!" << endl;
-    // run the simulation for 500 units of time
     SIMUL.run(10000);
 
     cout << endl << "#####################" << endl;
-    cout << "SOFTWARE TASKS" << endl;
-    cout << "\nTask1" << endl;
-    cout << "Maximum lateness:\t" << soft_rt_max1.getValue() << endl;
-    cout << "Mean lateness:\t" << soft_rt_mean1.getValue() << endl;
-    cout << "Minimum lateness:\t" << soft_rt_min1.getValue() << endl;
-    cout << "\nTask2" << endl;
-    cout << "Maximum lateness:\t" << soft_rt_max2.getValue() << endl;
-    cout << "Mean lateness:\t" << soft_rt_mean2.getValue() << endl;
-    cout << "Minimum lateness:\t" << soft_rt_min2.getValue() << endl;
-    cout << "\nTask3" << endl;
-    cout << "Maximum lateness:\t" << soft_rt_max3.getValue() << endl;
-    cout << "Mean lateness:\t" << soft_rt_mean3.getValue() << endl;
-    cout << "Minimum lateness:\t" << soft_rt_min3.getValue() << endl;
+    cout << "SOFTWARE TASKS\n" << endl;
+
+    for (unsigned int i=0; i<SW_TASK_NUM; ++i) {
+    cout << "Task" << i << " :" << '\t';
+    cout << "Lateness (min - mean - max):\t"
+         << soft_rt_max[i].getValue() << '\t'
+         << soft_rt_mean[i].getValue() << '\t'
+         << soft_rt_min[i].getValue() << endl;
+    }
     cout << "#####################" << endl << endl;
   } catch (BaseExc &e) {
     cout << e.what() << endl;
