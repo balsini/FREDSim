5b6fd6041acdb8e5448d74b02af95986c05bd4ce
Alessio Balsini <a.balsini@sssup.it>
2016-05-17 22:15:49 +0200

    Brought back average simulation

--- examples/accelerated_full/accelerated_full.cpp
+++ examples/accelerated_full/accelerated_full.cpp
@@ -503,7 +503,7 @@ int main(int argc, char * argv[])
                 string valDir = u_SW_Dir + "U_HW_" + to_string(arch.U_HW) + "/";
                 boost::filesystem::create_directories(valDir);
 
-#if 0
+#if 1
 
                 for (unsigned int i=0; i<arch.runs; ++i) {
 
